4a286fd7e754226e83e7b05502ea9e72c0bce32b
Seth Hamilton <seth.hamilton801@gmail.com>
2022-02-19 09:49:35 -0600

    package list that the automation engine uses

---
+++ seeker.py
@@ -0,0 +1,14 @@
+import random
+from typing import List, Tuple
+from cyberchase import Seeker
+
+class Seeker(Seeker):
+
+    def __init__(self):
+        pass
+
+
+    def get_action_from_state(self,board_states: List[List[int]],
+                              visible_squares: List[Tuple[int, int]],
+                              valid_moves: List[Tuple[int, int]]) -> Tuple[int, int]:
+        return random.choice(valid_moves)
